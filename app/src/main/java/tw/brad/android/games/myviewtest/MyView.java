ddf51b2b2b0cd295ddc95e4f248cb6c28e9e411c
Brad <brad@brad.tw>
2017-07-31 19:21:01 +0800

    2017073104

--- app/src/main/java/tw/brad/android/games/myviewtest/MyView.java
+++ app/src/main/java/tw/brad/android/games/myviewtest/MyView.java
@@ -1,9 +1,12 @@
 package tw.brad.android.games.myviewtest;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class MyView extends View {
@@ -13,5 +16,16 @@ public class MyView extends View {
         setBackgroundColor(Color.GREEN);
     }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
 
+        Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+        paint.setStrokeWidth(8);
+
+        canvas.drawCircle(100,100,30,paint);
+
+
+    }
 }
