b13ca9c09521ff91b86e0c6de220116ce6a740d3
Brad <brad@brad.tw>
2017-07-31 18:41:12 +0800

    2017073101

---
+++ app/src/main/java/tw/brad/android/games/myviewtest/MainActivity.java
@@ -0,0 +1,13 @@
+package tw.brad.android.games.myviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
