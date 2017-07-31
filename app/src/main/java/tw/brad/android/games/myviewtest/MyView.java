b96f132cf794848190d5815069abe2a08db3b287
Brad <brad@brad.tw>
2017-07-31 20:05:26 +0800

    2017073106

--- app/src/main/java/tw/brad/android/games/myviewtest/MyView.java
+++ app/src/main/java/tw/brad/android/games/myviewtest/MyView.java
@@ -10,12 +10,17 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class MyView extends View {
+    private LinkedList<HashMap<String,Float>> line;
 
     public MyView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         setBackgroundColor(Color.GREEN);
 
+        line = new LinkedList<>();
     }
 
     @Override
@@ -26,20 +31,17 @@ public class MyView extends View {
         paint.setColor(Color.BLUE);
         paint.setStrokeWidth(8);
 
-        canvas.drawCircle(100,100,30,paint);
-
-        canvas.drawLine(0,0,400,400,paint);
-
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_UP){
-            Log.i("brad", "UP");
-        }else if (event.getAction() == MotionEvent.ACTION_MOVE){
-            Log.i("brad", "MOVE");
-        }else if (event.getAction() == MotionEvent.ACTION_DOWN){
-            Log.i("brad", "DOWN");
+        float ex = event.getX(), ey = event.getY();
+        if (event.getAction() == MotionEvent.ACTION_MOVE ||
+                event.getAction() == MotionEvent.ACTION_DOWN){
+            HashMap<String,Float> point = new HashMap<>();
+            point.put("x", ex);
+            point.put("y", ey);
+            line.add(point);
         }
         return true;
     }
